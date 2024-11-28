5fa2448b9ecdfd62eb0622955dda88be9bf4c7b1
Pavel Golovin <sinon.1996@mail.ru>
2024-11-28 16:06:18 +0300

    practica12

---
+++ practica12/src/task_8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task_8 {
+    public void getKey() {
+        Scanner myScanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("Kлюч: ");
+                String key = myScanner.nextLine();
+                if (key.isEmpty()) {
+                    throw new Exception("Пустой ключ");
+                }
+                System.out.println("Данные для " + key);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка: " + e.getMessage());
+                System.out.println("Попробуйте снова.");
+            }
+        }
+        myScanner.close();
+    }
+}
